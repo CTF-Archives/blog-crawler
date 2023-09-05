93505cea9019b316ce43811c8aae3d7e3eb45f3c
Randark_JMT <randark@foxmail.com>
2023-09-05 14:33:35 +0000

    update

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # blog-crawler
 
+**请勿将此项目用于攻击/非法用途，此项目仅用于测试博客非侵入式爬取**
+
 此项目仅为雏形，目前仅对以下框架做了初步支持：
 
 - typecho
+
+未来预计支持以下框架：
+
+- csdn
+- cnblogs
+- wordpress
+- hugo
+- hexo
+- halo
+- vuepress
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import json
 from backend import typecho
 
-typecho.typecho_crawler("https://www.woshidie.com")
+
+def typecho_main():
+    with open("./source/typecho.json", "r") as f:
+        persons = json.load(f)["blog"]
+    for person in persons:
+        name = person["name"]
+        url = person["url"]
+        print(name)
+        typecho.typecho_crawler(url)
+
+
+if __name__ == "__main__":
+    typecho_main()
